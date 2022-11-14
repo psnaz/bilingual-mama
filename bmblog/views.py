12e18bbfc7420691af47e72b91bb2fbcb176bfc9
psnaz <psnazle@gmail.com>
2022-11-14 12:54:43 +0000

    Connect urls for view post detail

--- bmblog/views.py
+++ bmblog/views.py
@@ -17,7 +17,7 @@ class PostDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if post.likes.filter(id=self.request.user.id).exist():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
