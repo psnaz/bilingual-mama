f4304fe0ebb65557ff04a91ac89bf8852766e768
psnaz <psnazle@gmail.com>
2022-11-26 18:30:52 +0000

    Add query model to the admin and test

--- queries/admin.py
+++ queries/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Query
 
-# Register your models here.
+@admin.register(Query)
+class QueryAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'surname', 'submitted', 'answerdate')
+    list_filter = ('submitted', 'answerdate')
+    readonly_fields = ('id', 'submitted')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'surname', 'email', 'subject')
+        }),
+        ('Message Info', {
+            'classes': ('collapse',),
+            'fields': ('message', 'status', 'submitted')
+        }),
+        ('Query Admin', {
+            'classes': ('collapse',),
+            'fields': ('answerdate', 'username')
+        }),
+    )
