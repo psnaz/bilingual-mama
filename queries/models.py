5540f1ed0cd5f63975cd8e835300698b74a40aaa
psnaz <psnazle@gmail.com>
2022-11-26 17:06:32 +0000

    Create query model

--- queries/models.py
+++ queries/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('PARENT', 'I am a parent'),
+    ('EXPECTING', 'I am or we are expecting a baby'),
+    ('BLOGGER', 'I am a blogger'),
+    ('OTHER', 'I am just someone who wants to get in touch'),
+)
+
+class Query(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    subject = models.TextField(max_length=50)
+    message = models.TextField()
+    submitted = models.DateField(auto_now_add=True)
+    answerdate = models.DateField(blank=True, null=True)
+    username = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
